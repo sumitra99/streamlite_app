fe89be7fc2dd48b6d15970feaaf9e1a377c7f645
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-09-27 15:54:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit
 streamlit.title('conversation')
 streamlit.text('how r u ?')
 streamlit.text('had ur breakfast')
-streamlit.text('yeah,i ate soup 🥣')
+streamlit.text('yeah, soup 🥣')
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+streamlit.dataframe(my_fruit_list)
 
