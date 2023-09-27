f46d06bb8041fc1326bf58ed470d688924319797
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-09-27 16:12:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text('yeah, soup 🥣')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
-streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Apple','Banana'])
-#streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
 
 
