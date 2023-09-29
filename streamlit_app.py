3f04215b0e709caecfb481165061305600fa1b8f
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-09-29 12:30:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 import requests
-frutyvice_response=requests.get("https://fruityvice.com/api/fruit/"+'kiwi')
+frutyvice_response=requests.get("https://fruityvice.com/api/fruit/")
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 #streamlit.text(frutyvice_response.json())
