5a7cf7851740c018e08c472abcf5904e9a3c1828
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-09-29 12:32:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 import requests
-frutyvice_response=requests.get("https://fruityvice.com/api/fruit/")
+frutyvice_response=requests.get("https://fruityvice.com/api/fruit/kiwi")
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 #streamlit.text(frutyvice_response.json())
