c1edd9ca16afb67a9f01c3b15030166e1cba61f0
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-04 15:39:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,11 @@ st.write(df)
 
 st.sidebar.header("options")
 options_form = st.sidebar.form("options_form")
-user_name=options_form.text_input("name")
-user_age=options_form.text_input("age")
+user_name=options_form.text_input("Name")
+user_age=options_form.text_input("Age")
 add_data=options_form.form_submit_button()
 if add_data:
   st.write(user_name,user_age)
-  new_data=("name":user_name, "age":user_age)
+  new_data={"name":user_name, "age":int(user_age)}
   st.write(new_data)
   df=df.append()
