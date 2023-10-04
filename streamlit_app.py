aaa4ccafb022e0f43799e062fa2a6dabb38a7b7a
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-04 15:10:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,4 @@ import streamlit as st
 import pandas as pd
 st.title("APPLICATION")
 df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.table(df)
-
-
-
+st.write(df)
