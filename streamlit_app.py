e611fd8259843c62a2a600800c53305f8f16d6aa
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-04 15:30:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macr
 st.write(df)
 
 st.sidebar.header("options")
+options_form = st.sidebar.form("options_form")
+user_name=options_form.text_input("name")
+add_data=options_form.form_submit_button()
