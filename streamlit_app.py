ac0fd14c01867de4ee21a3d1ce255ce912bcd0dc
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-04 15:53:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 st.title("APPLICATION")
-df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.write(df)
+#df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#st.write(df)
 
 st.sidebar.header("options")
 options_form = st.sidebar.form("options_form")
