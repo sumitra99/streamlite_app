18f91d1fde4e336574f8907a6616f7a8e797700c
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-05 16:53:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("EXCEL DATA")
-df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#df=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.write(df)
 
 
@@ -12,7 +12,7 @@ user_age=options_form.text_input("age")
 add_data=options_form.form_submit_button()
 if add_data:
   st.write(user_name,user_age)
-  new_data={"name":user_name, "age":int(user_age)}
+  new_data=("name":user_name, "age":int(user_age))
   st.write(new_data)
   df=df.append(new_data,ignore_index=True)
   st.write(df)
