0dd0972473a847375b91be30baeb030a380bb30a
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-09 12:22:58 +0530

    Update streamlit_form.py

--- streamlit_form.py
+++ streamlit_form.py
@@ -4,6 +4,6 @@ st.title("Form")
 with st.form(key='form1'):
 name=st.text_input("Name")
 #slider_val=st.slider("form slider")
-submitted=st.form_submit_button()
+submit_button=st.form_submit_button()
 
 
