766ed181c6e53c8b901e80921b3d12a0d08fa421
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-12 13:48:13 +0530

    Update streamlit_form.py

--- streamlit_form.py
+++ streamlit_form.py
@@ -6,4 +6,6 @@ sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
 st.form(key='form1')
 name=st.text_input("Name")
 #submit_button=st.form_submit_button(label='signup')
+def click_button():
+    st.session_state.button = not st.session_state.button
 st.button('Click me', on_click=click_button)
