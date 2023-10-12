570981f1a075580f8220e02db8ebb2dd9da766d5
sumitra99 <104067803+sumitra99@users.noreply.github.com>
2023-10-12 13:36:01 +0530

    Update streamlit_form.py

--- streamlit_form.py
+++ streamlit_form.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Form")
-sepal_l = st.slider(‘Sepal lenght (cm)’, 1.0, 8.0, 0.5)
+sepal_l = st.slider(‘Sepal length (cm)’, 1.0, 8.0, 0.5)
 sepal_w = st.slider(‘Sepal width (cm)’, 2.0, 4.4, 0.5)
 st.form(key='form1')
 name=st.text_input("Name")
